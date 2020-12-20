e7167aef7cc0ccd8fa3cff7e3caf8600b5bdf941
myusron12 <ryusron19@gmail.com>
2020-12-20 23:31:27 +0700

    add tampilan home

--- app/src/main/java/com/example/applist/views/main/MainActivity.kt
+++ app/src/main/java/com/example/applist/views/main/MainActivity.kt
@@ -2,12 +2,44 @@ package com.example.applist.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.applist.R
+import com.example.applist.views.home.HomeFragment
+import com.example.applist.views.taskcomplete.TaskCompleteFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupbtmNavMain()
+    }
+
+    private fun setupbtmNavMain() {
+        btmNavMain.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    openFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_complete -> {
+                    openFragment(TaskCompleteFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
+
+        btmNavMain.selectedItemId = R.id.action_home
+    }
+
+    private fun openFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameMain, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
